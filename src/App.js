6f76febe378a210c262de8c0782293d0f0522162
Your Name <you@example.com>
2023-10-12 00:18:55 +0200

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,315 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from "react";
+import Papa from "papaparse";
+import {
+  Chart as ChartJS,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Filler,
+  Legend,
+  Colors,
+} from 'chart.js';
+import { Scatter } from 'react-chartjs-2';
+
+ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend, Filler, Colors)
+
+
+
+const tour = [
+  {
+    "Club": "Driver",
+    "Club-Speed(mph)": 113,
+    "Attack-Angle(deg)": "-1.3°",
+    "Ball-Speed(mph)": 167,
+    "Smash": 1.48,
+    "Factor(deg)": "10.9°",
+    "Launch-Ang.-Spin-Rate(rpm)": 2686,
+    "Max-Height(meters)": 29,
+    "Land-Angle(deg)": "38°",
+    "Carry(meters)": 251
+  },
+  {
+    "Club": "3 Wood",
+    "Club-Speed(mph)": 107,
+    "Attack-Angle(deg)": "-2.9°",
+    "Ball-Speed(mph)": 158,
+    "Smash": 1.48,
+    "Factor(deg)": "9.2°",
+    "Launch-Ang.-Spin-Rate(rpm)": 3655,
+    "Max-Height(meters)": 27,
+    "Land-Angle(deg)": "43°",
+    "Carry(meters)": 222
+  },
+  {
+    "Club": "5 Wood",
+    "Club-Speed(mph)": 103,
+    "Attack-Angle(deg)": "-3.3°",
+    "Ball-Speed(mph)": 152,
+    "Smash": 1.47,
+    "Factor(deg)": "9.4°",
+    "Launch-Ang.-Spin-Rate(rpm)": 4350,
+    "Max-Height(meters)": 28,
+    "Land-Angle(deg)": "47°",
+    "Carry(meters)": 210
+  },
+  {
+    "Club": "Hybrid",
+    "Club-Speed(mph)": 100,
+    "Attack-Angle(deg)": "-3.5°",
+    "Ball-Speed(mph)": 146,
+    "Smash": 1.46,
+    "Factor(deg)": "10.2°",
+    "Launch-Ang.-Spin-Rate(rpm)": 4437,
+    "Max-Height(meters)": 27,
+    "Land-Angle(deg)": "47°",
+    "Carry(meters)": 206
+  },
+  {
+    "Club": "3 Iron",
+    "Club-Speed(mph)": 98,
+    "Attack-Angle(deg)": "-3.1°",
+    "Ball-Speed(mph)": 142,
+    "Smash": 1.45,
+    "Factor(deg)": "10.4°",
+    "Launch-Ang.-Spin-Rate(rpm)": 4630,
+    "Max-Height(meters)": 25,
+    "Land-Angle(deg)": "46°",
+    "Carry(meters)": 194
+  },
+  {
+    "Club": "4 Iron",
+    "Club-Speed(mph)": 96,
+    "Attack-Angle(deg)": "-3.4°",
+    "Ball-Speed(mph)": 137,
+    "Smash": 1.43,
+    "Factor(deg)": "11.0°",
+    "Launch-Ang.-Spin-Rate(rpm)": 4836,
+    "Max-Height(meters)": 26,
+    "Land-Angle(deg)": "48°",
+    "Carry(meters)": 186
+  },
+  {
+    "Club": "5 Iron",
+    "Club-Speed(mph)": 94,
+    "Attack-Angle(deg)": "-3.7°",
+    "Ball-Speed(mph)": 132,
+    "Smash": 1.41,
+    "Factor(deg)": "12.1°",
+    "Launch-Ang.-Spin-Rate(rpm)": 5361,
+    "Max-Height(meters)": 28,
+    "Land-Angle(deg)": "49°",
+    "Carry(meters)": 177
+  },
+  {
+    "Club": "6 Iron",
+    "Club-Speed(mph)": 92,
+    "Attack-Angle(deg)": "-4.1°",
+    "Ball-Speed(mph)": 127,
+    "Smash": 1.38,
+    "Factor(deg)": "14.1°",
+    "Launch-Ang.-Spin-Rate(rpm)": 6231,
+    "Max-Height(meters)": 27,
+    "Land-Angle(deg)": "50°",
+    "Carry(meters)": 167
+  },
+  {
+    "Club": "7 Iron",
+    "Club-Speed(mph)": 90,
+    "Attack-Angle(deg)": "-4.3°",
+    "Ball-Speed(mph)": 120,
+    "Smash": 1.33,
+    "Factor(deg)": "16.3°",
+    "Launch-Ang.-Spin-Rate(rpm)": 7097,
+    "Max-Height(meters)": 29,
+    "Land-Angle(deg)": "50°",
+    "Carry(meters)": 157
+  },
+  {
+    "Club": "8 Iron",
+    "Club-Speed(mph)": 87,
+    "Attack-Angle(deg)": "-4.5°",
+    "Ball-Speed(mph)": 115,
+    "Smash": 1.32,
+    "Factor(deg)": "18.1°",
+    "Launch-Ang.-Spin-Rate(rpm)": 7998,
+    "Max-Height(meters)": 28,
+    "Land-Angle(deg)": "50°",
+    "Carry(meters)": 146
+  },
+  {
+    "Club": "9 Iron",
+    "Club-Speed(mph)": 85,
+    "Attack-Angle(deg)": "-4.7°",
+    "Ball-Speed(mph)": 109,
+    "Smash": 1.28,
+    "Factor(deg)": "20.4°",
+    "Launch-Ang.-Spin-Rate(rpm)": 8647,
+    "Max-Height(meters)": 27,
+    "Land-Angle(deg)": "51°",
+    "Carry(meters)": 135
+  },
+  {
+    "Club": "PW",
+    "Club-Speed(mph)": 83,
+    "Attack-Angle(deg)": "-5.0°",
+    "Ball-Speed(mph)": 102,
+    "Smash": 1.23,
+    "Factor(deg)": "24.2°",
+    "Launch-Ang.-Spin-Rate(rpm)": 9304,
+    "Max-Height(meters)": 27,
+    "Land-Angle(deg)": "52°",
+    "Carry(meters)": 124
+  }
+]
+
+function Metric(props) {
+  {
+    var average = props.avg.map(v => v.replace("°", ""))
+      .filter(v => v !== "")
+      .reduce((p, c) => parseFloat(p) + parseFloat(c), 0) / props.avg.length
+  }
+  return (
+    < div className="clubs" >
+      <p className="cold">{props.name}</p>
+      <p>{isNaN(+parseFloat(average).toFixed(1)) ? "" : +parseFloat(average).toFixed(1)}</p>
+      <p className="cold">{props.data[props.name]}</p>
+    </div >
+  );
+}
+
+
+function Clubb(props) {
+  var thisClub = props.values.filter(value => value[1] === props.data.Club);
+  if (thisClub.length === 0) {
+    return
+  }
+  return (
+    < div >
+      <h4>{props.data.Club}</h4>
+      <div className="container">
+        <Metric data={props.data} name={"Club-Speed(mph)"} avg={thisClub.map(x => x[4])} />
+        <Metric data={props.data} name={"Attack-Angle(deg)"} avg={thisClub.map(x => x[18])} />
+        <Metric data={props.data} name={"Ball-Speed(mph)"} avg={thisClub.map(x => x[5])} />
+        <Metric data={props.data} name={"Smash"} avg={thisClub.map(x => x[9])} />
+        {/* <Metric data={props.data} name={"Factor(deg)"} avg={thisClub.map(x => x[4])} /> */}
+        <Metric data={props.data} name={"Launch-Ang.-Spin-Rate(rpm)"} avg={thisClub.map(x => x[21])} />
+        <Metric data={props.data} name={"Max-Height(meters)"} avg={thisClub.map(x => x[11])} />
+        {/* <Metric data={props.data} name={"Land-Angle(deg)"} /> */}
+        <Metric data={props.data} name={"Carry(meters)"} avg={thisClub.map(x => x[6])} />
+        <div className='table'>
+          {<table>
+            <thead>
+              <tr>
+                {props.tableRows.map((rows, index) => {
+                  return <th key={index}>{rows}</th>;
+                })}
+              </tr>
+            </thead>
+            <tbody>
+              {thisClub.map((value, index) => {
+                return (
+                  <tr key={index}>
+                    {value.map((val, i) => {
+                      return <td key={i}>{val}</td>;
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>}
+        </div>
+
+      </div >
+    </div >
+  );
+}
+
+
 
 function App() {
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+
+  //State to store table Column name
+  const [tableRows, setTableRows] = useState([]);
+
+  //State to store the values
+  const [values, setValues] = useState([]);
+
+  var clubs = new Set(values.map(v => v[1]));
+
+  var data = {
+    datasets:
+      Array.from(clubs.values()).map(club => {
+        var group = values.filter(v => v[1] === club)
+        return {
+          label: club,
+          data: group.map(v => ({
+            x: v[15],
+            y: v[6],
+          })),
+        }
+      }).filter(v => v !== [])
+  };
+
+
+  const changeHandler = (event) => {
+    // Passing file data (event.target.files[0]) to parse using Papa.parse
+    Papa.parse(event.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      complete: function (results) {
+        const rowsArray = [];
+        const valuesArray = [];
+
+        // Iterating data to get column name and their values
+        results.data.map((d) => {
+          rowsArray.push(Object.keys(d));
+          valuesArray.push(Object.values(d));
+        });
+
+        // Filtered Column Names
+        setTableRows(rowsArray[0]);
+
+        // Filtered Values
+        setValues(valuesArray);
+      },
+    });
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {/* File Uploader */}
+        <input
+          type="file"
+          name="file"
+          onChange={changeHandler}
+          accept=".csv"
+          style={{ display: "block", margin: "10px auto" }}
+        />
+        <br />
+        <br />
+        {console.log(tableRows)}
+      </div>
+
+      <Scatter options={options} data={data} />
+
+      {/* {console.log(tour)} */}
+      {tour.map(x => <Clubb key={x.Club} data={x} values={values} tableRows={tableRows} />)}
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Tour.js
@@ -0,0 +1,134 @@
+const tour = [
+    {
+        "Club-Speed(mph)": "Driver",
+        "Attack-Angle(deg)": 113,
+        "Ball-Speed(mph)": "-1.3°",
+        "Smash": 167,
+        "Factor(deg)": 1.48,
+        "Launch-Ang.-Spin-Rate(rpm)": "10.9°",
+        "Max-Height(meters)": 2686,
+        "Land-Angle(deg)": 29,
+        "Carry(meters)": "38°"
+    },
+    {
+        "Club-Speed(mph)": "3-wood",
+        "Attack-Angle(deg)": 107,
+        "Ball-Speed(mph)": "-2.9°",
+        "Smash": 158,
+        "Factor(deg)": 1.48,
+        "Launch-Ang.-Spin-Rate(rpm)": "9.2°",
+        "Max-Height(meters)": 3655,
+        "Land-Angle(deg)": 27,
+        "Carry(meters)": "43°"
+    },
+    {
+        "Club-Speed(mph)": "5-wood",
+        "Attack-Angle(deg)": 103,
+        "Ball-Speed(mph)": "-3.3°",
+        "Smash": 152,
+        "Factor(deg)": 1.47,
+        "Launch-Ang.-Spin-Rate(rpm)": "9.4°",
+        "Max-Height(meters)": 4350,
+        "Land-Angle(deg)": 28,
+        "Carry(meters)": "47°"
+    },
+    {
+        "Club-Speed(mph)": "Hybrid(15-18°)",
+        "Attack-Angle(deg)": 100,
+        "Ball-Speed(mph)": "-3.5°",
+        "Smash": 146,
+        "Factor(deg)": 1.46,
+        "Launch-Ang.-Spin-Rate(rpm)": "10.2°",
+        "Max-Height(meters)": 4437,
+        "Land-Angle(deg)": 27,
+        "Carry(meters)": "47°"
+    },
+    {
+        "Club-Speed(mph)": "3-Iron",
+        "Attack-Angle(deg)": 98,
+        "Ball-Speed(mph)": "-3.1°",
+        "Smash": 142,
+        "Factor(deg)": 1.45,
+        "Launch-Ang.-Spin-Rate(rpm)": "10.4°",
+        "Max-Height(meters)": 4630,
+        "Land-Angle(deg)": 25,
+        "Carry(meters)": "46°"
+    },
+    {
+        "Club-Speed(mph)": "4-Iron",
+        "Attack-Angle(deg)": 96,
+        "Ball-Speed(mph)": "-3.4°",
+        "Smash": 137,
+        "Factor(deg)": 1.43,
+        "Launch-Ang.-Spin-Rate(rpm)": "11.0°",
+        "Max-Height(meters)": 4836,
+        "Land-Angle(deg)": 26,
+        "Carry(meters)": "48°"
+    },
+    {
+        "Club-Speed(mph)": "5-Iron",
+        "Attack-Angle(deg)": 94,
+        "Ball-Speed(mph)": "-3.7°",
+        "Smash": 132,
+        "Factor(deg)": 1.41,
+        "Launch-Ang.-Spin-Rate(rpm)": "12.1°",
+        "Max-Height(meters)": 5361,
+        "Land-Angle(deg)": 28,
+        "Carry(meters)": "49°"
+    },
+    {
+        "Club-Speed(mph)": "6-Iron",
+        "Attack-Angle(deg)": 92,
+        "Ball-Speed(mph)": "-4.1°",
+        "Smash": 127,
+        "Factor(deg)": 1.38,
+        "Launch-Ang.-Spin-Rate(rpm)": "14.1°",
+        "Max-Height(meters)": 6231,
+        "Land-Angle(deg)": 27,
+        "Carry(meters)": "50°"
+    },
+    {
+        "Club-Speed(mph)": "7-Iron",
+        "Attack-Angle(deg)": 90,
+        "Ball-Speed(mph)": "-4.3°",
+        "Smash": 120,
+        "Factor(deg)": 1.33,
+        "Launch-Ang.-Spin-Rate(rpm)": "16.3°",
+        "Max-Height(meters)": 7097,
+        "Land-Angle(deg)": 29,
+        "Carry(meters)": "50°"
+    },
+    {
+        "Club-Speed(mph)": "8-Iron",
+        "Attack-Angle(deg)": 87,
+        "Ball-Speed(mph)": "-4.5°",
+        "Smash": 115,
+        "Factor(deg)": 1.32,
+        "Launch-Ang.-Spin-Rate(rpm)": "18.1°",
+        "Max-Height(meters)": 7998,
+        "Land-Angle(deg)": 28,
+        "Carry(meters)": "50°"
+    },
+    {
+        "Club-Speed(mph)": "9-Iron",
+        "Attack-Angle(deg)": 85,
+        "Ball-Speed(mph)": "-4.7°",
+        "Smash": 109,
+        "Factor(deg)": 1.28,
+        "Launch-Ang.-Spin-Rate(rpm)": "20.4°",
+        "Max-Height(meters)": 8647,
+        "Land-Angle(deg)": 27,
+        "Carry(meters)": "51°"
+    },
+    {
+        "Club-Speed(mph)": "PW",
+        "Attack-Angle(deg)": 83,
+        "Ball-Speed(mph)": "-5.0°",
+        "Smash": 102,
+        "Factor(deg)": 1.23,
+        "Launch-Ang.-Spin-Rate(rpm)": "24.2°",
+        "Max-Height(meters)": 9304,
+        "Land-Angle(deg)": 27,
+        "Carry(meters)": "52°"
+    }
+]
